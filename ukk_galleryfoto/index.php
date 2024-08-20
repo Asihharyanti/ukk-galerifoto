9587675cf0c82e463713dffdc9a7ace98fdef5e4
Asihharyanti <151506365+Asihharyanti@users.noreply.github.com>
2024-08-20 01:25:07 -0700

    Add files via upload

---
+++ ukk_galleryfoto/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Website Gallery Foto</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container">
+    <a class="navbar-brand" href="index.php">Website Gallery Foto</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse mt-2" id="navbarNavAltMarkup">
+      <div class="navbar-nav me-auto">
+        
+      </div>
+      <a href="register.php" class="btn btn-outline-primary m-1">Daftar</a>
+      <a href="login.php" class="btn btn-outline-primary m-1">Masuk</a>
+    </div>
+  </div>
+</nav>
+
+<div class="container mt-3">
+    <div class="row">
+        <div class="col-md-3">
+            <div class="card">
+                <img src="path/to/image.jpg" class="card-img-top" title="Deskripsi Gambar" style="height:12rem;">
+                <div class="card-footer text-center">
+                    <a href="#" class="btn btn-outline-primary">10 suka</a>
+                    <a href="#" class="btn btn-outline-primary">3 komentar</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<footer class="d-flex justify-content-center border-top mt-3 bg-light">
+    <p>&copy; UKK Gallery Foto | asih</p>
+</footer>
+
+<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+</body>
+</html>
